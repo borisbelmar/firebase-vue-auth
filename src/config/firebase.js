25ab2ede3728f5db19c33a7614ee26a7727aebc1
Boris Belmar <borisbelmarm@gmail.com>
2019-07-16 20:26:46 -0400

    Probando algo

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    //Aquí van los datos de firebase
+    apiKey: "xxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx"
+};
+
+module.exports = firebaseConfig;
